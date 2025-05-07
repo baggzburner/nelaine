84d577da9c59e9748fcb6c49a40fd322e57f1ee5
baggzburner <baggzburner@gmail.com>
2025-05-07 11:12:30 +0300

    feature products review

--- src/data/FeatureProducts.jsx
+++ src/data/FeatureProducts.jsx
@@ -1,41 +1,39 @@
-import p1 from '../assets/p1.jpg'
-import p2 from '../assets/p2.jpg'
-import p3 from '../assets/p3.jpg'
-import p11 from '../assets/p11.jpg'
+// src/data/featureProducts.js
 
-
-
+import p1 from '../assets/p1.jpg';
+import p2 from '../assets/p2.jpg';
+import p3 from '../assets/p3.jpg';
+import p11 from '../assets/p11.jpg';
 
 const featureProducts = [
-    {
-      id: 1,
-      name: 'Palazzo pants',
-      description: 'Perfect blend of comfort and luxury for your chill days.',
-      price: 1650,
-      image: p1
-    },
-    {
-      id: 2,
-      name: 'Palazzo pants',
-      description: 'Stylish enough to wear out, comfy enough to sleep in.',
-      price: 1650,
-      image: p2
-    },
-    {
-      id: 3,
-      name: 'Palazzo pants',
-      description: 'A soft pastel robe to wrap yourself in elegance.',
-      price: 1650,
-      image: p3
-    },
-    {
-      id: 3,
-      name: 'Palazzo pants',
-      description: 'A soft pastel robe to wrap yourself in elegance.',
-      price: 1650,
-      image: p11
-    }
-  ];
-  
-  export default featureProducts;
-  
+  {
+    id: 1,
+    name: 'Palazzo pants',
+    description: 'Perfect blend of comfort and luxury for your chill days.',
+    price: 1650,
+    image: p1,
+  },
+  {
+    id: 2,
+    name: 'Palazzo pants',
+    description: 'Stylish enough to wear out, comfy enough to sleep in.',
+    price: 1650,
+    image: p2,
+  },
+  {
+    id: 3,
+    name: 'Palazzo pants',
+    description: 'A soft pastel robe to wrap yourself in elegance.',
+    price: 1650,
+    image: p3,
+  },
+  {
+    id: 4,
+    name: 'Palazzo pants',
+    description: 'A soft pastel robe to wrap yourself in elegance.',
+    price: 1650,
+    image: p11,
+  },
+];
+
+export default featureProducts;
