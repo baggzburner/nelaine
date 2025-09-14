244bb7f4251dd5bab77fabecda029c2d766053ac
baggzburner <baggzburner@gmail.com>
2025-09-14 16:48:49 +0300

    cta update

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 
-function Cta({ selectedProduct }) {
+function Cta() {
   return (
     <div className='bg-dark text-light text-center py-4'>
       <h1 className='my-5'>Make Your Order</h1>
       <p className='my-5'>Quality, Comfort and Style are our key focus factors</p>
       
-      {/* Button triggers modal */}
+      {/* Button triggers CTA modal */}
       <button
         className='btn btn-warning mt-3 mb-5 fw-bold'
         data-bs-toggle="modal"
-        data-bs-target="#contactModal"
+        data-bs-target="#ctaContactModal"   // updated ID
       >
         Make Your Order
       </button>
 
-      {/* Modal */}
+      {/* CTA Modal */}
       <div
         className="modal fade"
-        id="contactModal"
+        id="ctaContactModal"   // updated ID
         tabIndex="-1"
-        aria-labelledby="contactModalLabel"
+        aria-labelledby="ctaContactModalLabel"
         aria-hidden="true"
       >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="contactModalLabel">
+              <h5 className="modal-title" id="ctaContactModalLabel">
                 Contact Seller
               </h5>
               <button
@@ -37,18 +37,10 @@ function Cta({ selectedProduct }) {
               ></button>
             </div>
             <div className="modal-body">
-              {selectedProduct ? (
-                <>
-                  <p><strong>Product:</strong> {selectedProduct.name}</p>
-                  <p><strong>Price:</strong> kshs. {selectedProduct.price}</p>
-                  <hr />
-                  <p><strong>Seller:</strong> NelaineComfyWear</p>
-                  <p><strong>Phone:</strong> +2547123456789</p>
-                  <p><strong>Email:</strong> nelaine@sales.com</p>
-                </>
-              ) : (
-                <p>Please select a product before ordering.</p>
-              )}
+              <hr />
+              <p><strong>Seller:</strong> NelaineComfyWear</p>
+              <p><strong>Phone:</strong> +2547123456789</p>
+              <p><strong>Email:</strong> nelaine@sales.com</p>
             </div>
             <div className="modal-footer">
               <button
